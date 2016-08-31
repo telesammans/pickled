b6af845ed2c21bc0c6d1f40757d801a56a37c7ac
alfred <alfred@nurd.nu>
2016-08-31 20:56:07 +0000

    completing fork, setting dist identifiers

--- features/hostname/post-install.sh
+++ features/hostname/post-install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-echo 'pindestino' >work/etc/hostname
-sed -i 's/raspberrypi/pindestino/' work/etc/hosts
+echo 'pickled' >work/etc/hostname
+sed -i 's/raspberrypi/pickled/' work/etc/hosts
 
 rm work/etc/os-release.orig
 cat <<_EOF_ >work/etc/os-release
-PRETTY_NAME="Pindestino GNU/Linux 1"
-NAME="Pindestino GNU/Linux"
+PRETTY_NAME="Pickled GNU/Linux 1 (nissa)"
+NAME="Pickled GNU/Linux"
 VERSION_ID="1"
-VERSION="1"
-ID=pindestino
+VERSION="1 (nissa)"
+ID=pickled
 ID_LIKE=debian
 ANSI_COLOR="1;31"
-HOME_URL="http://klandestino.github.io/pindestino/"
-SUPPORT_URL="https://github.com/klandestino/pindestino"
-BUG_REPORT_URL="https://github.com/klandestino/pindestino/issues"
+HOME_URL="https://github.com/telesammans/pickled"
+SUPPORT_URL="https://github.com/telesammans/pickled"
+BUG_REPORT_URL="https://github.com/telesammans/pickled"
 _EOF_
 
-echo 'Pindestino GNU/Linux 1\n \l' >work/etc/issue
+echo 'Pickled GNU/Linux 1 \n \l' >work/etc/issue
 echo '' >>work/etc/issue
-echo 'Pindestino GNU/Linux 1' >work/etc/issue.net
+echo 'Pickled GNU/Linux 1' >work/etc/issue.net
 rm work/etc/issue.orig
 rm work/etc/issue.net.orig
 
